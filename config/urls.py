08da873eaa8e6b9a921e052c0ff40938ad1f0219
Ksu <kseniyaartemova@yandex.ru>
2023-08-22 15:07:21 +0200

    fixed urls, set admin, started vies

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('mailing.urls')),
+    path('', include('mailing.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from mailing.models import Client, Message, Log, Mailing
 
 # Create your views here.
+
+
+class HomeView(CreateView):
+    model = Mailing
+    template_name = 'mailing/home.html'
+    fields = ('timedate', 'frequency', 'status', 'message')
+

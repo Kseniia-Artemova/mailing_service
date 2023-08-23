ee40f90ea1880d26a485242493177ad9186eb526
Ksu <kseniyaartemova@yandex.ru>
2023-08-23 19:05:50 +0200

    fixed input of contacts on the home page

--- mailing/views.py
+++ mailing/views.py
@@ -10,3 +10,12 @@ class HomeView(CreateView):
     template_name = 'mailing/home.html'
     fields = ('timedate', 'frequency', 'status', 'message')
 
+    def post(self, request, *args, **kwargs):
+        print(request.POST.getlist('contacts'))
+        print(request.POST.get('frequency'))
+        print(request.POST.get('subject'))
+        print(request.POST.get('body'))
+        print(request.POST.get('schedule_time'))
+        print(request.POST.get('schedule_date'))
+
+        return super().post(request, *args, **kwargs)

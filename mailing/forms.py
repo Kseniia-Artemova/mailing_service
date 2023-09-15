010a7eb857b4e13618ceb974af33691e6a92fe1f
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-09-15 14:49:20 +0200

    created forms (#4)

--- mailing/forms.py
+++ mailing/forms.py
@@ -1,8 +1,9 @@
 from django import forms
-from mailing.models import Client
+from mailing.models import Client, Message, Mailing
 
 
 class ClientForm(forms.ModelForm):
+
     last_name = forms.CharField()
     first_name = forms.CharField()
     father_name = forms.CharField(required=False)
@@ -10,3 +11,17 @@ class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
         fields = ('email', 'comment')
+
+
+class MessageForm(forms.ModelForm):
+
+    class Meta:
+        model = Message
+        fields = '__all__'
+
+
+class MailingForm(forms.ModelForm):
+
+    class Meta:
+        model = Mailing
+        exclude = ('status',)

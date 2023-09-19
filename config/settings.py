543037a28697da34976f43973f319cbe3b7d25e2
Ksu <kseniyaartemova@yandex.ru>
2023-09-19 13:27:27 +0200

    moved base.html in root/templates, corrected address in all templates

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'mailing' / 'static',
+    BASE_DIR / 'static',
 ]
 
 # Default primary key field type

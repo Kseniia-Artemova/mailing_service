8a448a3b48051cc9dfc5ae11be1fbd08cb5cf029
Ksu <kseniyaartemova@yandex.ru>
2023-09-20 18:15:48 +0200

    created login, logout and registration controllers, configured verification by mail

---
+++ users/services.py
@@ -0,0 +1,12 @@
+from django.core.mail import send_mail
+from config import settings
+
+
+def send_verification_url(email, url):
+    send_mail(
+        subject='Вам выслана ссылка для верификации почтового адреса!',
+        message=f'Пожалуйста, пройдите по этой ссылке для окончания регистрации на сайте:\n'
+                f'{url}',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[email]
+    )
--- users/views.py
+++ users/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+import secrets
 
-# Create your views here.
+from django.contrib.auth.views import LoginView as BaseLoginView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView
+
+from users import services
+from users.forms import LoginForm, UserRegisterForm
+from users.models import User
+
+
+class LoginView(BaseLoginView):
+    template_name = 'users/login.html'
+    form_class = LoginForm
+
+
+class RegisterView(CreateView):
+    model = User
+    template_name = 'users/registration.html'
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            self.object = form.save(commit=False)
+            verification_code = secrets.token_urlsafe(nbytes=11)
+            self.object.verification_code = verification_code
+
+            url = reverse('users:verification', args=[verification_code])
+            absolute_url = self.request.build_absolute_uri(url)
+            services.send_verification_url(email=self.object.email,
+                                           url=absolute_url)
+            self.object.save()
+
+        return super().form_valid(form)
+
+
+def verification(request, verification_code):
+
+    user = User.objects.get(verification_code=verification_code)
+    user.is_active = True
+    user.save()
+    return redirect(reverse('users:login'))
+

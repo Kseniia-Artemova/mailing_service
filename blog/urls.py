d3718f536afc10775defdb261219e4215219fdc9
Ksu <kseniyaartemova@yandex.ru>
2023-09-22 14:39:29 +0200

    added templates for controllers of blog: create, update, list

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,9 @@ from blog.views import BlogEntryListView, BlogEntryCreateView, BlogEntryDeleteVi
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('', BlogEntryListView.as_view(), name='blog_list'),
-    path('create_entry', BlogEntryCreateView.as_view(), name='create_entry'),
-    path('delete_entry', BlogEntryDeleteView.as_view(), name='delete_entry'),
-    path('update_entry/<int:pk>', BlogEntryUpdateView.as_view(), name='update_entry'),
+    path('', BlogEntryListView.as_view(), name='blog_entry_list'),
+    path('create_entry/', BlogEntryCreateView.as_view(), name='create_entry'),
+    path('delete_entry/<int:pk>/', BlogEntryDeleteView.as_view(), name='delete_entry'),
+    path('update_entry/<int:pk>/', BlogEntryUpdateView.as_view(), name='update_entry'),
+    path('entry_detail/<int:pk>/', BlogEntryUpdateView.as_view(), name='entry_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,41 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+
+from blog.forms import BlogEntryForm
+from blog.models import BlogEntry
 
 
 class BlogEntryListView(ListView):
-    pass
+    model = BlogEntry
+    template_name = 'blog/blog_entry_list.html'
 
 
 class BlogEntryCreateView(CreateView):
-    pass
+    model = BlogEntry
+    form_class = BlogEntryForm
+    template_name = 'blog/blog_entry_form.html'
+    success_url = reverse_lazy('blog:blog_entry_list')
+    extra_context = {
+        'title': 'Создать запись блога',
+        'button': 'Создать',
+    }
 
 
 class BlogEntryUpdateView(UpdateView):
-    pass
+    model = BlogEntry
+    form_class = BlogEntryForm
+    template_name = 'blog/blog_entry_form.html'
+    success_url = reverse_lazy('blog:blog_entry_list')
+    extra_context = {
+        'title': 'Изменить запись блога',
+        'button': 'Сохранить',
+    }
 
 
 class BlogEntryDeleteView(DeleteView):
+    pass
+
+
+class BlogEntryDetailView(DetailView):
     pass

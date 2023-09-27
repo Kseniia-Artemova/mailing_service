dee75f83703611b2f0a9b0f87530e16e9fa113f8
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-09-28 01:43:37 +0200

    Develop (#12)
    * little changing in settings
    
    * created models
    
    * completed with models
    
    * Feature/creation views (#2)
    
    * fixed urls, set admin, started vies
    
    * created template of home page
    
    * created base template
    
    * fixed input of contacts on the home page
    
    * added name for contacts in form
    
    * created template for recipient_form
    
    * created template for recipients list
    
    * added CRUD for Client
    
    * fixed CRUD-controllers for Client
    
    * in the process of creating templates and controllers for Mailing
    
    * little fix
    
    * proces of working with view of update_mailing, just for saving
    
    * fixed models (#3)
    
    * created forms (#4)
    
    * Feature/fix views (#5)
    
    * attempt to do formset, in process
    
    * fixed forms and views for creation mailing and messages
    
    * moved base.html in root/templates, corrected address in all templates
    
    * fixed display of buttons for deleting, fixed display of date and status for list of mailings
    
    * added DetailView for Mailing with template
    
    * slightly improved the display of mailing list and buttons
    
    * removed the halo from the buttons
    
    * did not complete the task in the crontab, in progress
    
    * crontab works now, first phase is completed
    
    * Feature/autentication (#6)
    
    * created new model of User, run migrations, created command for creation of superuser
    
    * created login, logout and registration controllers, configured verification by mail
    
    * added message about verification code after registration; added LoginRequiredMixins to mailing controllers
    
    * added authorization, created custom commands for mailing, hidden buttons and functionality for anonymous users
    
    * Feature/blog (#7)
    
    * created app blog, created model BlogEntry, controllers in process
    
    * added templates for controllers of blog: create, update, list
    
    * fixed buttons, fixed functions in mailing/services.py, fixed verification
    
    * working on UserForm and profile in process
    
    * created controllers and templates for deletion blog entry and view blog entry
    
    * finished interface of blog, still without manager group
    
    * created command for creation of group 'managers'; added logic of restrictions and permissions for the technical specifications managers; corrected the templates according to this (#8)
    
    * Feature/cache (#9)
    
    * fixed display of mailing and clients for managers
    
    * configured controller caching and low-level caching
    
    * added key environment variables to the configuration file, added .env.sample
    
    * added docstrings and typehints (#10)
    
    * updated requirements.txt, uploaded fixture fixture_data.json
    
    * Update README.md
    
    * Update README.md
    
    * little change on location of card in home page
    
    * Update README.md
    
    * Update README.md
    
    * changed Exeption in mailing.services.send_mailing (#11)
    
    * Update README.md

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv('.env')
+dotenv_path = BASE_DIR / '.env'
+load_dotenv(dotenv_path=dotenv_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -26,7 +27,7 @@ load_dotenv('.env')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -41,7 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'mailing'
+    'mailing',
+    'blog',
+    'users',
+    'django_crontab',
+    'django_dump_load_utf8',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +64,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'users.context_processors.is_manager',
             ],
         },
     },
@@ -118,6 +124,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+CRONJOBS = [
+    ('*/5 * * * *', 'mailing.services.change_status_to_started'),
+    ('*/5 * * * *', 'mailing.services.send_mails_regular'),
+]
+
+CRONTAB_COMMAND_SUFFIX = f'>> {BASE_DIR / "crontab_log.log"} 2>&1'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -125,7 +137,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'mailing' / 'static',
+    BASE_DIR / 'static',
 ]
 
 # Default primary key field type
@@ -135,3 +147,24 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+LOGIN_URL = '/users/'
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL') == 'True'
+
+CACHE_ENABLED = os.getenv('CACHE_ENABLED') == 'True'
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": os.getenv('LOCATION'),
+        "TIMEOUT": 120,
+    }
+}
--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-import settings
+from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mailing.urls'))
+    path('', include('mailing.urls')),
+    path('user/', include('users.urls')),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
